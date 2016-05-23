dd0ece60733904440d25f6ceb297805b3250fe0b
Duane Leem <duaneleem@alumni.ggu.edu>
2016-05-23 12:02:50 -0700

    Added angular directives.

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+"use strict";
+
+angular.module("appROSI", ["ui.router", "ngResource"])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            // Route: Home
+            .state("app", {
+                url: "/",
+                views: {
+                    "header": {
+                        templateUrl: "assets/views/header.html",
+                    }, // header
+                    "content": {
+                        templateUrl: "assets/views/home.html",
+                    }, // content
+                    "footer": {
+                        templateUrl: "assets/views/footer.html",
+                    }
+                } // views
+            }) // .state
+
+            // Route: ROSI
+            .state("app.rosi", {
+                url: "rosi",
+                views: {
+                    "content@": {
+                        templateUrl: "assets/views/rosi.html",
+                    } // content
+                } // views
+            }) // state
+
+            // Route: Become an ASP
+            .state("app.become_asp", {
+                url: "become_asp",
+                views: {
+                    "content@": {
+                        templateUrl: "assets/views/become-asp.html",
+                    } // content
+                } // views
+            }); // state
+
+        $urlRouterProvider.otherwise("/");
+    }) // .config
+; // angular.module
