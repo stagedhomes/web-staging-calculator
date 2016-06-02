2c9e7f0dfc95deb679ac12e65315acb7d85ace42
Duane Leem <duaneleem@alumni.ggu.edu>
2016-06-02 09:51:53 -0700

    ng-app.js had a problem with ui-router.  Redid syntax and that worked.

--- assets/js/ng-app.js
+++ assets/js/ng-app.js
@@ -1,55 +1,53 @@
-"use strict";
+(function() {
+    "use strict";
 
-angular.module("appROSI", ["ui.router", "ngResource"])
-    .config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            // Route: Home
-            .state("app", {
-                url: "/",
-                views: {
-                    "header": {
-                        templateUrl: "/assets/views/header.html",
-                    }, // header
-                    "content": {
-                        templateUrl: "/assets/views/home.html",
-                    }, // content
-                    "footer": {
-                        templateUrl: "/assets/views/footer.html",
-                    } // footer
-                } // views
-            }) // .state
+    angular.module("appROSI", ["ui.router", "ngResource"])
+        .config(function($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                // Route: Home
+                .state("app", {
+                    url: "/",
+                    views: {
+                        "header": {
+                            templateUrl: "/assets/views/header.html",
+                        }, // header
+                        "content": {
+                            templateUrl: "/assets/views/home.html",
+                        } // content
+                    } // views
+                }) // .state
 
-            // Route: Results
-            .state("app.results", {
-                url: "results",
-                templateUrl: "/assets/views/results.html",
-                views: {
-                    "content@": {
-                        templateUrl: "/assets/views/results.html",
-                    } // content
-                } // views
-            }) // state
+                // Route: Results
+                .state("app.results", {
+                    url: "results",
+                    views: {
+                        "content@": {
+                            templateUrl: "/assets/views/results.html"
+                        } // content
+                    } // views
+                }) // .state
 
-            // Route: ROSI
-            .state("app.rosi", {
-                url: "rosi",
-                views: {
-                    "content@": {
-                        templateUrl: "/assets/views/rosi.html",
-                    } // content
-                } // views
-            }) // state
+                // Route: ROSI
+                .state("app.rosi", {
+                    url: "rosi",
+                    views: {
+                        "content@": {
+                            templateUrl: "/assets/views/rosi.html"
+                        } // content
+                    } // views
+                }) // state
 
-            // Route: Become an ASP
-            .state("app.become_asp", {
-                url: "become_asp",
-                views: {
-                    "content@": {
-                        templateUrl: "/assets/views/become-asp.html",
-                    } // content
-                } // views
-            }); // state
+                // Route: Become an ASP
+                .state("app.become_asp", {
+                    url: "become_asp",
+                    views: {
+                        "content@": {
+                            templateUrl: "/assets/views/become-asp.html"
+                        } // content
+                    } // views
+                }); // state
 
-        // $urlRouterProvider.otherwise("/");
-    }) // .config
-; // angular.module
+            $urlRouterProvider.otherwise("/");
+        }) // .config
+    ; // angular.module
+})(); // function
